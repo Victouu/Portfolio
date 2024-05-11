a9af56f459b9a150e7d3ddfc444e9c64a9e0d9f3
Victouu <rouevictor@gmail.com>
2024-05-11 21:03:11 +0200

    emailsend

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
-import { useTheme } from "next-themes";
 import { useState, useEffect } from "react";
+import axios from "axios";
 //composants
 import Image from "next/image";
 //import photo
@@ -9,21 +9,167 @@ import Image from "next/image";
 import Head from "../components/head";
 
 function Contact() {
-  const { theme, resolvedTheme } = useTheme();
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [formData, setFormData] = useState({
+    nom: "",
+    email: "",
+    sujet: "",
+    message: "",
+  });
 
-  useEffect(() => {
-    setIsDarkTheme(resolvedTheme === "dark");
-  }, [resolvedTheme]);
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await axios.post("/emailSend", formData); // Assurez-vous que cette URL correspond à l'URL de votre backend
+      alert("Votre message a été envoyé avec succès !");
+      // Réinitialisez le formulaire après l'envoi réussi si nécessaire
+      setFormData({
+        nom: "",
+        email: "",
+        sujet: "",
+        message: "",
+      });
+    } catch (error) {
+      console.error("Erreur lors de l'envoi du message :", error);
+      alert(
+        "Une erreur s'est produite lors de l'envoi du message. Veuillez réessayer plus tard."
+      );
+    }
+  };
   return (
     <div className="flex min-h-screen w-screen flex-col items-center">
       <div className="flex h-screen w-[90%] max-w-[90%] flex-col md:max-w-7xl overflow-hidden">
         <Head />
-        <div className="md:max-w-2/3 mb-20 flex w-full flex-col items-center space-y-4 text-sm md:space-y-11 md:pb-0 md:text-lg">
-          <h2 className="w-full text-xs font-extralight uppercase tracking-widest dark:text-blue md:text-xl text-tuscan text-left">Contact</h2>  
-          <h2 className="whitespace-pre-line dark:text-blue text-tuscan text-md xs:text-2xl md:text-6xl font-extrabold">
-              </h2> 
+        <div className="w-full max-w-4xl mx-auto py-12 md:py-16 px-4 md:px-6">
+          <div className="grid md:grid-cols-2 gap-8 md:gap-12">
+            <div className="space-y-4">
+              <h2 className="text-3xl font-bold">Contactez-moi</h2>
+              <p className="text-gray-500 dark:text-gray-400">
+                N&apos;hésitez pas à me contacter pour toute demande ou
+                question.
+              </p>
+              <div className="space-y-2">
+                <div className="flex items-center space-x-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="h-5 w-5 text-gray-500 dark:text-gray-400"
+                  >
+                    <path d="M22 17a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V9.5C2 7 4 5 6.5 5H18c2.2 0 4 1.8 4 4v8Z"></path>
+                    <polyline points="15,9 18,9 18,11"></polyline>
+                    <path d="M6.5 5C9 5 11 7 11 9.5V17a2 2 0 0 1-2 2v0"></path>
+                    <line x1="6" x2="7" y1="10" y2="10"></line>
+                  </svg>
+                  <span className="text-gray-500 dark:text-gray-400">
+                    rouevictor@gmail.com
+                  </span>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="h-5 w-5 text-gray-500 dark:text-gray-400"
+                  >
+                    <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
+                  </svg>
+                  <span className="text-gray-500 dark:text-gray-400">
+                    07.68.09.75.02
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div>
+              <form className="space-y-4" onSubmit={handleSubmit}>
+                <div className="space-y-2">
+                  <label
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    htmlFor="name"
 
+                  >
+                    Nom
+                  </label>
+                  <input
+                    className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                    id="name"
+                    placeholder="Entrez votre nom"
+                    value={formData.nom} onChange={handleChange}
+                  />
+                </div>
+                <div className="space-y-2">
+                  <label
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    htmlFor="email"
+                  >
+                    Email
+                  </label>
+                  <input
+                    className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                    id="email"
+                    placeholder="Entrez votre email"
+                    type="email"
+                    value={formData.email} onChange={handleChange}
+                  />
+                </div>
+                <div className="space-y-2">
+                  <label
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    htmlFor="subject"
+                  >
+                    Sujet
+                  </label>
+                  <input
+                    className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                    id="subject"
+                    placeholder="Entrez le sujet"
+                    value={formData.sujet} onChange={handleChange} 
+                  />
+                </div>
+                <div className="space-y-2">
+                  <label
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    htmlFor="message"
+                  >
+                    Message
+                  </label>
+                  <textarea
+                    className="flex w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 min-h-[150px]"
+                    id="message"
+                    placeholder="Entrez votre message"
+                    value={formData.message} onChange={handleChange}
+                  ></textarea>
+                </div>
+                <button
+                  className=" border-slate-950 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground h-10 px-4 py-2 w-full"
+                  type="submit"
+                >
+                  Envoyer le message
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </div>
