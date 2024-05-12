820935b850c6ccfc9732d5f8755ef0c8c3d91fda
Victouu <rouevictor@gmail.com>
2024-05-12 15:09:03 +0200

    modif next confg

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/contact',
+          destination: '/mailsender',
+          permanent: false, // rediriger temporairement
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
